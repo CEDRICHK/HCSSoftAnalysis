43be8be8a005f169572d8cb5e788224e785b8e35
CEDUSER <cedric.hassen-khodja@mri.cnrs.fr>
2017-09-08 18:58:05 +0200

    init project

---
+++ R/hello.R
@@ -0,0 +1,17 @@
+# Hello, world!  This is an example function named 'hello' which
+# prints 'Hello, world!'.  You can learn more about package
+# authoring with RStudio at: http://r-pkgs.had.co.nz/ Some
+# useful keyboard shortcuts for package authoring: Build and
+# Reload Package: 'Ctrl + Shift + B' Check Package: 'Ctrl +
+# Shift + E' Test Package: 'Ctrl + Shift + T'
+
+
+#' \code{hello} prints "Hello, world!"
+#' @param NULL
+#' @return Print the character "Hello, world!"
+#' @examples
+#' hello()
+hello <- function() {
+  print("Hello, world!")
+}
+
